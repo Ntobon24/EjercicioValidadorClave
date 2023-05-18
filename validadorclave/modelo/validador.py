7168276334070532f1ed434e1efd80fa120b185e
Nicolas Tobon <nicolastobon242>
2023-05-18 13:47:28 -0500

    mayus, min, num hechas

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -7,16 +7,31 @@ class ReglaValidacion(ABC):
         self.longitud_esperada = longitud_esperada
 
     def _validar_longitud(self, clave: str) -> bool:
-        pass
+        if len(clave) <= self.longitud_esperada:
+            return False
+        else:
+            return True
 
     def _contiene_mayuscula(self, clave: str) -> bool:
-        pass
+        for caracter in clave:
+            if caracter.isupper():
+                return True
+
+        return False
 
     def _contiene_minuscula(self, clave: str) -> bool:
-        pass
+        for caracter in clave:
+            if caracter.islower():
+                return True
+
+        return False
 
     def _contiene_numero(self, clave: str) -> bool:
-        pass
+        for caracter in clave:
+            if caracter.isdigit():
+                return True
+
+        return False
 
     @abstractmethod
     def es_valida(self, clave: str) -> bool:
@@ -25,11 +40,16 @@ class ReglaValidacion(ABC):
 
 class ReglaValidacionGanimedes(ReglaValidacion):
 
-    def __init__(self, longitud_esperada: int):
-        super().__init__(longitud_esperada)
+    def __init__(self):
+        super().__init__(longitud_esperada=8)
 
     def contiene_caracter_especial(self, clave: str) -> bool:
-        pass
+        caracteres_especiales = ["@", "_", "#", "$", "%"]
+        for caracter in clave:
+            if caracter in caracteres_especiales:
+                return True
+            else:
+                return False
 
     def es_valida(self, clave: str) -> bool:
         pass
@@ -37,8 +57,8 @@ class ReglaValidacionGanimedes(ReglaValidacion):
 
 class ReglaValidacionCalisto(ReglaValidacion):
 
-    def __init__(self, longitud_esperada: int):
-        super().__init__(longitud_esperada)
+    def __init__(self):
+        super().__init__(longitud_esperada=6)
 
     def contiene_calisto(self, clave: str) -> bool:
         pass
