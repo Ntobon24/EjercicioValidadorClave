93ffe06be9722b04a3395319427ed42034d74004
Nicolas Tobon <nicolastobon242>
2023-05-18 12:56:48 -0500

    Estructura finalizada

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,56 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+
+class ReglaValidacion(ABC):
+
+    def __init__(self, longitud_esperada: int):
+        self.longitud_esperada = longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        pass
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        pass
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        pass
+
+    def _contiene_numero(self, clave: str) -> bool:
+        pass
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+
+    def __init__(self, longitud_esperada: int):
+        super().__init__(longitud_esperada)
+
+    def contiene_caracter_especial(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class ReglaValidacionCalisto(ReglaValidacion):
+
+    def __init__(self, longitud_esperada: int):
+        super().__init__(longitud_esperada)
+
+    def contiene_calisto(self, clave: str) -> bool:
+        pass
+
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
+class Validador:
+
+    def __init__(self, regla: ReglaValidacion):
+        self.regla = regla
+
+    def es_valida(self, clave: str) -> bool:
+        pass
